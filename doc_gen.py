3ec5a64cb13813c2bb55e1ee1abcbca7e9f273c9
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-06-26 18:08:27 +0200

    tkinter form creation
    Initialize form

--- doc_gen.py
+++ doc_gen.py
@@ -2,5 +2,7 @@ from docxtpl import DocxTemplate
 
 doc = DocxTemplate("invoice_template.docx")     # Template File
 
-doc.render({"name" : "John"})                   # Example
+invoice_list = [[2, "item", 0.5, 1]]
+
+doc.render({"name" : "John", "invoice_list" : invoice_list})                   # Example of fields to be inserted into template
 doc.save("new_invoice.docx")                    # New File
